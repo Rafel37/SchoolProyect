60a79924365528540bb3b2c3cdca2e98d5c1e739
root <rafelperez37@hotmail.es>
2019-03-06 18:05:48 +0100

    hola

--- src/basedatos/DatosBaseDatos.java
+++ src/basedatos/DatosBaseDatos.java
@@ -6,9 +6,9 @@ public interface DatosBaseDatos {
     public static String TAB_INICIO_COL_NOMBRE             = "NOMBRE";
     public static String TAB_INICIO_COL_APELLIDO           = "APELLIDOS";
     public static String TAB_INICIO_COL_CORREO             = "CORREO";
-    public static String TAB_INICIO_COL_CONTRASENIA        = "CONTRASENIA";
-    public static String TAB_CICLOS_COL_NOMBRE_CICLO       = "NOMBRE_CICLO";
-    public static String TAB_CICLOS_COL_CONOCIMIENTOPREVIO = "CONOCIMIENTO_PREVIO";
+    public static String TAB_INICIO_COL_PASS               = "PASS";
+    public static String TAB_CICLOS_COL_CICLO              = "CICLO";
+    public static String TAB_CICLOS_COL_CONOCIMIENTO       = "CONOCIMIENTO";
     public static String TAB_TABLACICLOS                   = "CICLOS";
     public static String TAB_CICLOS_ID_ALUMNO              = "ID_ALUMNO";
 }
